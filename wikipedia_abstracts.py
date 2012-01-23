aea225a794d9388c5f70e4d3fcd17877acb9ab9e
Olivier Grisel <olivier.grisel@ensta.org>
2012-01-23 18:48:06 +0100

    first working commit

--- wikipedia_abstracts.py
+++ wikipedia_abstracts.py
@@ -1,100 +1,74 @@
-"""Utility script to extract the text abstract of Wikipedia from DBpedia
+"""Utility script to extract the text abstract of Wikipedia from DBpedia"""
+# License: MIT
 
-The unquote method is taken from rdflib (c) Sean B. Palmer, inamidst.com
-and distributed under the GPL 2, W3C, BSD, or MIT licenses :
-https://code.google.com/p/rdflib/source/browse/rdflib/plugins/parsers/ntriples.py
+import os
+import re
+from urllib import unquote
+from bz2 import BZ2File
 
-The rest of the script is distributed under the BSD or MIT licenses as well.
-"""
+URL_PATTERN = ("http://downloads.dbpedia.org/"
+               "{version}/{lang}/long_abstracts_{lang}.nt.bz2")
+VERSION = "3.7"
+LANG = "en"
+LOCAL_FOLDER = os.path.join("~", "data", "dbpedia")
 
-uriref = b(r'<([^:]+:[^\s"<>]+)>')
-literal = b(r'"([^"\\]*(?:\\.[^"\\]*)*)"')
-litinfo = b(r'(?:@([a-z]+(?:-[a-z0-9]+)*)|\^\^') + uriref + b(r')?')
-
-r_line = re.compile(b(r'([^\r\n]*)(?:\r\n|\r|\n)'))
-r_wspace = re.compile(b(r'[ \t]*'))
-r_wspaces = re.compile(b(r'[ \t]+'))
-r_tail = re.compile(b(r'[ \t]*\.[ \t]*'))
-r_uriref = re.compile(uriref)
-r_nodeid = re.compile(b(r'_:([A-Za-z][A-Za-z0-9]*)'))
-r_literal = re.compile(literal + litinfo)
-
-bufsiz = 2048
-validate = False
-
-class Node(unicode): pass
-
-class ParseError(Exception): pass
-
-class Sink(object):
-    def __init__(self):
-        self.length = 0
-
-    def triple(self, s, p, o):
-        self.length += 1
-        print (s, p, o)
-
-quot = {b('t'): u'\t', b('n'): u'\n', b('r'): u'\r', b('"'): u'"', b('\\'): u'\\'}
-r_safe = re.compile(b(r'([\x20\x21\x23-\x5B\x5D-\x7E]+)'))
-r_quot = re.compile(b(r'\\(t|n|r|"|\\)'))
-r_uniquot = re.compile(b(r'\\u([0-9A-F]{4})|\\U([0-9A-F]{8})'))
+LINE_PATTERN = re.compile(r'<([^<]+?)> <[^<]+?> "(.*)"@\w\w .\n')
 
 
-def unquote(s):
-    """Unquote an N-Triples string."""
-    if not validate:
-        return s.decode('unicode-escape')
-    else:
-        result = []
-        while s:
-            m = r_safe.match(s)
-            if m:
-                s = s[m.end():]
-                result.append(m.group(1).decode('ascii'))
+def fetch(lang=LANG, version=VERSION, folder=LOCAL_FOLDER):
+    """Fetch the DBpedia abstracts dump and cache it locally"""
+    folder = os.path.expanduser(folder)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    url = URL_PATTERN.format(**locals())
+    filename = url.rsplit('/', 1)[-1]
+    filename = os.path.join(folder, filename)
+    if not os.path.exists(filename):
+        print "Downloading %s to %s" % (url, filename)
+        # for some reason curl is much faster than urllib2 and has the
+        # additional benefit of progress report and streaming the data directly
+        # to the hard drive
+        cmd = "curl -o %s %s" % (filename, url)
+        os.system(cmd)
+    return filename
+
+
+def human_readable(id, prefix="http://dbpedia.org/resource/"):
+    return unquote(id[len(prefix):]).replace('_', ' ')
+
+
+def extract_abstracts(filename, max_items=None, verbose=True):
+    """Extract and decode abstracts on the fly
+
+    Return a generator of (id, title, text) triples:
+    - id is the raw DBpedia URI of the resource.
+    - title that should match the Wikipedia title of the article.
+    - text is the first paragraph of the Wikipedia article without any markup.
+
+    """
+    reader = BZ2File if filename.endswith('.bz2') else open
+
+    current_line_number = 0
+    with reader(filename, 'rb') as f:
+        for line in f:
+            current_line_number += 1
+            if max_items is not None and current_line_number > max_items:
+                break
+            if verbose and current_line_number % 10000 == 0:
+                print "Decoding line %d" % current_line_number
+            m = LINE_PATTERN.match(line)
+            if m is None:
+                if verbose:
+                    print ("[WARNING] Invalid line %d, skipping."
+                           % current_line_number)
                 continue
+            id = m.group(1)
+            title = human_readable(id)
+            text = m.group(2).decode('unicode-escape')
+            yield (id, title, text)
 
-            m = r_quot.match(s)
-            if m:
-                s = s[2:]
-                result.append(quot[m.group(1)])
-                continue
 
-            m = r_uniquot.match(s)
-            if m:
-                s = s[m.end():]
-                u, U = m.groups()
-                codepoint = int(u or U, 16)
-                if codepoint > 0x10FFFF:
-                    raise ParseError("Disallowed codepoint: %08X" % codepoint)
-                result.append(unichr(codepoint))
-            elif s.startswith(b('\\')):
-                raise ParseError("Illegal escape at: %s..." % s[:10])
-            else: raise ParseError("Illegal literal character: %r" % s[0])
-        return u''.join(result)
-
-        result = []
-        while s:
-            m = r_safe.match(s)
-            if m:
-                s = s[m.end():]
-                result.append(m.group(1).decode('ascii'))
-                continue
-
-            m = r_quot.match(s)
-            if m:
-                s = s[2:]
-                result.append(quot[m.group(1)])
-                continue
-
-            m = r_uniquot.match(s)
-            if m:
-                s = s[m.end():]
-                u, U = m.groups()
-                codepoint = int(u or U, 16)
-                if codepoint > 0x10FFFF:
-                    raise ParseError("Disallowed codepoint: %08X" % codepoint)
-                result.append(unichr(codepoint))
-            elif s.startswith(b('\\')):
-                raise ParseError("Illegal escape at: %s..." % s[:10])
-            else: raise ParseError("Illegal literal character: %r" % s[0])
-        return u''.join(result)
+if __name__ == "__main__":
+    for id, title, text in extract_abstracts(fetch(), max_items=10):
+        print "%s\n\n%s\n\n" % (title, text)
