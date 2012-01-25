a6ac29c4bd35b9d2613470d880c95e677f9beab8
Olivier Grisel <olivier.grisel@ensta.org>
2012-01-25 17:26:41 +0100

    extract_link + switched to logging API

--- dbpediakit.py
+++ dbpediakit.py
@@ -1,12 +1,20 @@
 """Utility script to tweak the ntriples DBpedia dumps"""
 # License: MIT
 
-import os
-import re
 import csv
+import logging
+import os
+import sys
+import re
 from collections import namedtuple
 from urllib import unquote
 from bz2 import BZ2File
+
+
+# use stderr for progress monitoring to be able to use stdout for piping CSV
+# stream to postgresql for instance
+logging.basicConfig(stream=sys.stderr, level=logging.INFO,
+                    format='%(levelname)s\t%(asctime)s\t%(message)s')
 
 URL_PATTERN = ("http://downloads.dbpedia.org/"
                "{version}/{lang}/{archive_name}_{lang}.nt.bz2")
@@ -15,6 +23,7 @@ LANG = "en"
 LOCAL_FOLDER = os.path.join("~", "data", "dbpedia")
 
 TEXT_LINE_PATTERN = re.compile(r'<([^<]+?)> <[^<]+?> "(.*)"@(\w\w) .\n')
+LINK_LINE_PATTERN = re.compile(r'<([^<]+?)> <([^<]+?)> <([^<]+?)> .\n')
 
 
 article = namedtuple('article', ('id', 'title', 'text', 'lang'))
@@ -22,7 +31,13 @@ link = namedtuple('link', ('source', 'target'))
 
 
 def fetch(archive_name, lang=LANG, version=VERSION, folder=LOCAL_FOLDER):
-    """Fetch the DBpedia abstracts dump and cache it locally"""
+    """Fetch the DBpedia abstracts dump and cache it locally
+
+    Archive name is the filename part without the language, for instance:
+      - long_abstracts to be parsed with extract_text
+      - skos_categories to be parsed with extract_link
+
+    """
     folder = os.path.expanduser(folder)
     if not os.path.exists(folder):
         os.makedirs(folder)
@@ -40,40 +55,86 @@ def fetch(archive_name, lang=LANG, version=VERSION, folder=LOCAL_FOLDER):
     return filename
 
 
-def human_readable(id, prefix="http://dbpedia.org/resource/"):
-    return unquote(id[len(prefix):]).replace('_', ' ')
+def extract_link(archive_filename, max_items=None, predicate_filter=None,
+                 strip_prefix="http://dbpedia.org/resource/"):
+    """Extract link information on the fly
 
+    Predicate filter can be a single string or a collection of strings
+    to filter out triples that don't match.
 
-def extract_text(filename, max_items=None, min_length=300, verbose=True):
-    """Extract and decode text literals on the fly
-
-    Return a generator of article(id, title, text) named tuples:
-    - id is the raw DBpedia URI of the resource.
-    - title that should match the Wikipedia title of the article.
-    - text is the first paragraph of the Wikipedia article without any markup.
-    - lang is the language code of the text literal
+    Return a generator of link(source, target) named tuples.
 
     """
-    reader = BZ2File if filename.endswith('.bz2') else open
+    reader = BZ2File if archive_filename.endswith('.bz2') else open
 
     current_line_number = 0
     extracted = 0
 
-    with reader(filename, 'rb') as f:
+    if predicate_filter is not None:
+        if isinstance(predicate_filter, basestring):
+            predicate_filter = set([predicate_filter])
+        else:
+            predicate_filter = set(predicate_filter)
+
+    with reader(archive_filename, 'rb') as f:
         for line in f:
             current_line_number += 1
             if max_items is not None and extracted > max_items:
                 break
-            if verbose and current_line_number % 10000 == 0:
-                print "Decoding line %d" % current_line_number
+            if current_line_number % 10000 == 0:
+                logging.info("Decoding line %d", current_line_number)
+            m = LINK_LINE_PATTERN.match(line)
+            if m is None:
+                logging.warn("Invalid line %d, skipping.",
+                             current_line_number)
+                continue
+            predicate = m.group(2)
+            if (predicate_filter is not None
+                and not predicate in predicate_filter):
+                continue
+            source = m.group(1)
+            target = m.group(3)
+            if strip_prefix is not None:
+                source = source[len(strip_prefix):]
+                target = target[len(strip_prefix):]
+
+            yield article(source, target)
+            extracted += 1
+
+
+def extract_text(archive_filename, max_items=None, min_length=300,
+                 strip_prefix="http://dbpedia.org/resource/"):
+    """Extract and decode text literals on the fly
+
+    Return a generator of article(id, title, text) named tuples:
+    - id is the raw DBpedia id of the resource (without the resource prefix).
+    - title is the decoded id that should match the Wikipedia title of the
+      article.
+    - text is the first paragraph of the Wikipedia article without any markup.
+    - lang is the language code of the text literal
+
+    """
+    reader = BZ2File if archive_filename.endswith('.bz2') else open
+
+    current_line_number = 0
+    extracted = 0
+
+    with reader(archive_filename, 'rb') as f:
+        for line in f:
+            current_line_number += 1
+            if max_items is not None and extracted > max_items:
+                break
+            if current_line_number % 10000 == 0:
+                logging.info("Decoding line %d", current_line_number)
             m = TEXT_LINE_PATTERN.match(line)
             if m is None:
-                if verbose:
-                    print ("[WARNING] Invalid line %d, skipping."
-                           % current_line_number)
+                logging.warn("Invalid line %d, skipping.",
+                             current_line_number)
                 continue
             id = m.group(1)
-            title = human_readable(id)
+            if strip_prefix:
+                id = id[len(strip_prefix):]
+            title = unquote(id).replace('_', ' ')
             text = m.group(2).decode('unicode-escape')
             if len(text) < min_length:
                 continue
