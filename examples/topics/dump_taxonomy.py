bd91ce591e4eada64e4d0dd51f2e16e8319fad29
Olivier Grisel <olivier.grisel@ensta.org>
2012-05-03 16:25:28 +0200

    make it possible to use simple TSV for dump scripts

--- examples/topics/dump_taxonomy.py
+++ examples/topics/dump_taxonomy.py
@@ -4,6 +4,7 @@ import sys
 import os.path
 import dbpediakit.postgres as pg
 
+USE_QUOTED_CSV = False
 
 if len(sys.argv) > 1:
     output = "'%s'" % os.path.abspath(sys.argv[1])
@@ -23,7 +24,11 @@ FROM (
 ) AS d
 GROUP BY d.id
 """
-copy_query = "COPY (%s) TO %s WITH (FORMAT CSV, FORCE_QUOTE *);" % (
-    select_query, output)
+if USE_QUOTED_CSV:
+    copy_query = "COPY (%s) TO %s WITH (FORMAT CSV, FORCE_QUOTE *);" % (
+        select_query, output)
+else:
+    copy_query = "COPY (%s) TO %s;" % (select_query, output)
+
 print copy_query
 pg.execute(copy_query)
--- examples/topics/dump_taxonomy_examples.py
+++ examples/topics/dump_taxonomy_examples.py
@@ -4,6 +4,7 @@ import sys
 import os.path
 import dbpediakit.postgres as pg
 
+USE_QUOTED_CSV = False
 
 if len(sys.argv) > 1:
     output = "'%s'" % os.path.abspath(sys.argv[1])
@@ -16,7 +17,11 @@ SELECT g.id, array_to_string(g.grounded_topics, ' '), la.text
 FROM grouped_taxonomy_articles g, long_abstracts la
 WHERE g.id = la.id
 """
-copy_query = "COPY (%s) TO %s WITH (FORMAT CSV, FORCE_QUOTE *);" % (
-    select_query, output)
+
+if USE_QUOTED_CSV:
+    copy_query = "COPY (%s) TO %s WITH (FORMAT CSV, FORCE_QUOTE *);" % (
+        select_query, output)
+else:
+    copy_query = "COPY (%s) TO %s;" % (select_query, output)
 print copy_query
 pg.execute(copy_query)
