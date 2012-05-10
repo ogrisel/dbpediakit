f0c25a2c31f916c0de51323c44ac8c14125e3de6
Olivier Grisel <olivier.grisel@ensta.org>
2012-05-10 18:54:25 +0200

    unify all building scripts with CLI option switches

--- examples/topics/build_taxonomy.py
+++ examples/topics/build_taxonomy.py
@@ -7,12 +7,10 @@ Running this script will probably take around 30 minutes.
 # License: MIT
 
 import logging
-from os.path import join, sep
+from os.path import join, sep, abspath
 import dbpediakit.postgres as pg
 
-FOLDER = __file__.rsplit(sep, 1)[0]
-max_items = None  # set to None for processing the complete dumps
-max_depth = 0
+SQL_SCRIPTS_FOLDER = __file__.rsplit(sep, 1)[0]
 
 
 def candidate_article_processor(tuples):
@@ -20,44 +18,154 @@ def candidate_article_processor(tuples):
         yield (source, target, source[len("Category:"):])
 
 
-updated = pg.check_link_table(
-    "redirects", "redirects",
-    predicate_filter="http://dbpedia.org/ontology/wikiPageRedirects",
-    max_items=max_items)
+def check_load_taxonomy_data(max_items=None):
+    """Load the tables from the source archives
 
-updated &= pg.check_link_table(
-    "skos_categories", "categories",
-    predicate_filter="http://www.w3.org/2004/02/skos/core#broader",
-    columns=(
-        ('id', True),
-        ('broader', True),
-        ('candidate_article', True),
-    ),
-    processor=candidate_article_processor,
-    max_items=max_items)
+    Leave max_items to None for processing the complete dumps.
+    """
+    updated = pg.check_link_table(
+        "redirects", "redirects",
+        predicate_filter="http://dbpedia.org/ontology/wikiPageRedirects",
+        max_items=max_items)
 
-updated &= pg.check_link_table(
-    "article_categories", "article_categories",
-    predicate_filter="http://purl.org/dc/terms/subject")
+    updated &= pg.check_link_table(
+        "skos_categories", "categories",
+        predicate_filter="http://www.w3.org/2004/02/skos/core#broader",
+        columns=(
+            ('id', True),
+            ('broader', True),
+            ('candidate_article', True),
+        ),
+        processor=candidate_article_processor,
+        max_items=max_items)
 
-updated &= pg.check_text_table(
-    "long_abstracts", "long_abstracts", max_items=max_items)
+    updated &= pg.check_link_table(
+        "article_categories", "article_categories",
+        predicate_filter="http://purl.org/dc/terms/subject")
 
-if updated:
-    pg.execute("ANALYZE")
+    if updated:
+        pg.execute("ANALYZE")
 
 
-# Load some aggregation function to manipulate arrays of arrays (to materialize
-# paths to the roots in the taxonomy)
-pg.check_run_if_undef(join(FOLDER, "array_aggregate.sql"))
+def check_load_examples_data(max_items):
+    """Load the abstract data
 
-# Aggregate categories info and find semantic grounding by trying to match with
-# wikipedia articles
-pg.check_run_if_undef(join(FOLDER, "build_grounded_categories.sql"))
-pg.check_run_if_undef(join(FOLDER, "init_taxonomy.sql"))
+    Leave max_items to None for processing the complete dumps.
+    """
+    updated = pg.check_text_table(
+        "long_abstracts", "long_abstracts", max_items=max_items)
+    if updated:
+        pg.execute("ANALYZE")
 
-current_depth = int(pg.select("SELECT max(depth) from taxonomy_dag"))
-if current_depth < max_depth:
-    for depth in range(current_depth + 1, max_depth + 1):
-        logging.info("Growing taxonomy to depth=%d", depth)
-        pg.check_run_if_undef(join(FOLDER, "grow_taxonomy.sql"))
+
+def grow_taxonomy(max_depth=1):
+    # Load some aggregation function to manipulate arrays of arrays
+    # (to materialize paths to the roots in the taxonomy)
+    pg.check_run_if_undef(join(SQL_SCRIPTS_FOLDER, "array_aggregate.sql"))
+
+    # Aggregate categories info and find semantic grounding by trying to match
+    # with wikipedia articles
+    pg.check_run_if_undef(join(SQL_SCRIPTS_FOLDER,
+                               "build_grounded_categories.sql"))
+    pg.check_run_if_undef(join(SQL_SCRIPTS_FOLDER, "init_taxonomy.sql"))
+
+    current_depth = int(pg.select("SELECT max(depth) from taxonomy_dag"))
+    if current_depth < max_depth:
+        for depth in range(current_depth + 1, max_depth + 1):
+            logging.info("Growing taxonomy to depth=%d", depth)
+            pg.check_run_if_undef(join(SQL_SCRIPTS_FOLDER,
+                                       "grow_taxonomy.sql"))
+
+
+def dump_results(filename, query, format='tsv'):
+    filepath = abspath(filename)
+    if format == 'csv':
+        copy_query = "COPY (%s) TO '%s' WITH (FORMAT CSV, FORCE_QUOTE *);" % (
+            query, filepath)
+    elif format == 'tsv':
+        copy_query = "COPY (%s) TO '%s';" % (query, filepath)
+
+    # TODO: use a subprocess to dump to stdout and pipe to file from the
+    # python process to be able to use the same unix user and avoid file
+    # permission issues
+    pg.execute(copy_query)
+
+
+def dump_taxonomy(filename, format='tsv'):
+    """"""
+    query = """\
+    SELECT
+      d.id,
+      string_agg(d.grounded_broader, ' ' ORDER BY d.grounded_broader),
+      first_agg(d.article)
+    FROM (
+      SELECT DISTINCT id, grounded_broader, article
+      FROM taxonomy_dag
+      WHERE grounded = 't'
+    ) AS d
+    GROUP BY d.id
+    """
+    dump_results(filename, query, format=format)
+
+
+def dump_examples(filename, format='tsv'):
+    query = """\
+    SELECT g.id, array_to_string(g.grounded_topics, ' '), la.text
+    FROM grouped_taxonomy_articles g, long_abstracts la
+    WHERE g.id = la.id
+    """
+    dump_results(filename, query, format=format)
+
+
+if __name__ == "__main__":
+
+    import argparse
+    all_operations = (
+        'build_taxonomy',
+        'build_examples',
+        'dump_taxonomy',
+        'dump_examples',
+    )
+
+    parser = argparse.ArgumentParser(
+        description='Build a taxonomy and example text documents')
+
+    parser.add_argument(
+        '--operations', nargs='+',
+        choices=all_operations,
+        default=all_operations,
+        help='Sequence of operations to execute.')
+
+    parser.add_argument(
+        '--taxonomy-file',
+        default='dbpedia-taxonomy.tsv',
+        help='Filename to store the TSV export of the taxonomy.')
+
+    parser.add_argument(
+        '--examples-file', default='dbpedia-examples.tsv',
+        help='Filename to store the TSV export of the examples text'
+        ' categorized using the taxonomy.')
+
+    parser.add_argument(
+        '--max-depth', default=1, type=int,
+        help='Limit the depth of subcategories to follow from the roots.',
+    )
+
+    parser.add_argument(
+        '--max-items', default=None, type=int,
+        help='Limit the number of rows to load from DBpedia archives'
+        ' (for debug purpose only)')
+
+    args = parser.parse_args()
+    for operation in args.operations:
+        if operation == 'build_taxonomy':
+            check_load_taxonomy_data(args.max_items)
+            grow_taxonomy(args.max_depth)
+        elif operation == 'build_examples':
+            check_load_examples_data(args.max_items)
+            pg.check_run_if_undef(
+                join(SQL_SCRIPTS_FOLDER, "build_dataset.sql"))
+        elif operation == 'dump_taxonomy':
+            dump_taxonomy(args.taxonomy_file)
+        elif operation == 'dump_examples':
+            dump_examples(args.examples_file)
